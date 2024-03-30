adf268f87fe6e313f811eac3d5303ed1df4344ec
Tomash Ghz <ghz.tomash@hotmail.com>
2024-03-30 02:49:19 +0100

    Initial repo

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+use netstat2::{get_sockets_info, AddressFamilyFlags, ProtocolFlags, ProtocolSocketInfo};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let af_flags = AddressFamilyFlags::IPV4 | AddressFamilyFlags::IPV6;
+    let proto_flags = ProtocolFlags::TCP | ProtocolFlags::UDP;
+    let sockets_info = get_sockets_info(af_flags, proto_flags)?;
+    
+    for si in sockets_info {
+        match si.protocol_socket_info {
+            ProtocolSocketInfo::Tcp(tcp_si) => println!(
+                "TCP {}:{} -> {}:{} {:?} - {}",
+                tcp_si.local_addr,
+                tcp_si.local_port,
+                tcp_si.remote_addr,
+                tcp_si.remote_port,
+                si.associated_pids,
+                tcp_si.state
+            ),
+            ProtocolSocketInfo::Udp(udp_si) => println!(
+                "UDP {}:{} -> *:* {:?}",
+                udp_si.local_addr, udp_si.local_port, si.associated_pids
+            ),
+        }
+    }
+
+    Ok(())
+}
