b9be500e8a911825382be8f25c84be7b007ffb2b
Tomash Ghz <ghz.tomash@hotmail.com>
2024-04-02 22:21:09 +0200

    chore: renames flags for numeric outputs

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,9 @@ use sysinfo::{Pid, System};
 mod services;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let show_host_name = true; // true: show numeric addresses, false: show resolved addresses
-    let show_service_name = true; // true: show service name, false: show port number
-    let show_process_name = true; // true: show process name, false: show process ID
+    let numeric_host = false; // true: show numeric addresses, false: show resolved addresses
+    let numeric_service = false; // true: show service name, false: show port number
+    let numeric_process = false; // true: show process name, false: show process ID
 
     let af_flags = AddressFamilyFlags::IPV4;
     // let af_flags = AddressFamilyFlags::IPV4 | AddressFamilyFlags::IPV6;
@@ -21,35 +21,40 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     for si in sockets_info {
         match si.protocol_socket_info {
             ProtocolSocketInfo::Tcp(tcp_si) => {
-                let local_addr = if !show_host_name {
+                let local_addr = if numeric_host {
                     format!("{}", tcp_si.local_addr)
                 } else {
                     let ip = tcp_si.local_addr;
                     let host = lookup_addr(&ip).unwrap_or(ip.to_string());
                     format!("{}", host)
                 };
-                let remote_addr = if !show_host_name {
+                let remote_addr = if numeric_host {
                     format!("{}", tcp_si.remote_addr)
                 } else {
                     let ip = tcp_si.remote_addr;
                     let host = lookup_addr(&ip).unwrap_or(ip.to_string());
                     format!("{}", host)
                 };
-                let local_port = if !show_service_name {
+                let local_port = if numeric_service {
                     format!("{}", tcp_si.local_port)
                 } else {
                     let port = tcp_si.local_port;
                     let service = services::lookup_service(port).unwrap_or(port.to_string());
                     format!("{}", service)
                 };
-                let remote_port = if !show_service_name {
+                let remote_port = if numeric_service {
                     format!("{}", tcp_si.remote_port)
                 } else {
                     let port = tcp_si.remote_port;
                     let service = services::lookup_service(port).unwrap_or(port.to_string());
                     format!("{}", service)
                 };
-                let process_name = if show_process_name {
+                let process_name = if numeric_process {
+                    si.associated_pids
+                        .iter()
+                        .map(|pid| pid.to_string())
+                        .collect::<Vec<String>>()
+                } else {
                     let pids = si.associated_pids;
                     let process = pids
                         .iter()
@@ -62,11 +67,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         })
                         .collect::<Vec<String>>();
                     process
-                } else {
-                    si.associated_pids
-                        .iter()
-                        .map(|pid| pid.to_string())
-                        .collect::<Vec<String>>()
                 };
                 println!(
                     "TCP {}:{} -> {}:{} {:?} - {}",
